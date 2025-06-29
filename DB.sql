e43ea693f36d865399b2e0c3c3d828c2ad90b4fb
yunzivv <yunzivv@gmail.com>
2025-06-29 15:54:05 +0900

    테이블 수정

--- DB.sql
+++ DB.sql
@@ -17,14 +17,134 @@ CREATE TABLE article(
 );
 
 # 게시물 테이블 데이터 추가
-INSERT INTO article
-SET regDate = NOW(),
-    updateDate = NOW(),
-    title = CONCAT('제목', SUBSTRING(RAND() *1000 FROM 1 FOR 2)),
-    `body` = CONCAT('내용', SUBSTRING(RAND() *1000 FROM 1 FOR 2)),
-    memberId = FLOOR(1 + RAND() * 5),
-    boardId = FLOOR(1 + RAND() * 3),
-    certId = 1;
+CREATE TABLE board (
+                       id TINYINT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+                       `code` VARCHAR(20) NOT NULL,
+                       `name` VARCHAR(20) NOT NULL,
+                       regDate DATETIME NOT NULL,
+                       updateDate DATETIME NOT NULL,
+                       delStatus TINYINT(1) NOT NULL DEFAULT 0 COMMENT '0 = 존재, 1 = 삭제',
+                       delDate DATETIME
+);
+
+
+# 리액션 테이블
+CREATE TABLE `comment` (
+                           id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+                           memberId INT UNSIGNED NOT NULL,
+                           relTypeCode VARCHAR(20) NOT NULL,
+                           relId INT UNSIGNED NOT NULL,
+                           `body` TEXT NOT NULL,
+                           regDate DATETIME NOT NULL,
+                           updateDate DATETIME NOT NULL
+);
+
+
+# 게시판 테이블
+CREATE TABLE reaction (
+                          id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+                          relTypeCode VARCHAR(20) NOT NULL COMMENT '관련 데이터 타입 코드',
+                          relId INT UNSIGNED NOT NULL COMMENT '관련 데이터 id',
+                          memberId INT UNSIGNED NOT NULL,
+                          `point` TINYINT(1) NOT NULL COMMENT '1 = 좋아요, 0 = 좋아요 취소',
+                          regDate DATETIME NOT NULL,
+                          updateDate DATETIME NOT NULL
+);
+
+# 자격증 시험 테이블
+CREATE TABLE exam (
+                      id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+                      certId INT UNSIGNED NOT NULL,
+                      category CHAR(10) NOT NULL,
+                      examDate DATE NOT NULL,
+                      regDate DATETIME NOT NULL,
+                      updateDate DATETIME NOT NULL
+);
+
+# 자격증 시험 문제 테이블
+CREATE TABLE questions (
+                           id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+                           certId INT UNSIGNED,
+                           examId INT UNSIGNED,
+                           subjectId INT UNSIGNED,
+                           questNum SMALLINT UNSIGNED NOT NULL,
+                           `body` TEXT,
+                           hasImage BOOLEAN DEFAULT FALSE,
+                           imgUrl VARCHAR(255),
+                           regDate DATETIME NOT NULL,
+                           updateDate DATETIME NOT NULL,
+
+                           INDEX idx_exam_subject (examId, subjectId)
+);
+
+
+# 자격증 시험 보기 테이블
+CREATE TABLE choices (
+                         id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+                         questId INT UNSIGNED NOT NULL,
+                         label TINYINT UNSIGNED NOT NULL,
+                         `body` TEXT NOT NULL,
+                         isCorrect BOOLEAN NOT NULL,
+                         regDate DATETIME NOT NULL,
+                         updateDate DATETIME NOT NULL
+);
+
+
+# 자격증 문제 테이블
+CREATE TABLE certificate (
+                             id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+                             `name` VARCHAR(50) NOT NULL,
+                             certGrade INT UNSIGNED,
+                             isNational TINYINT COMMENT '0=국가공인민간자격 1 = 국가기술자격, -1 = 민간자격',
+                             agency VARCHAR(100),
+                             parentId INT UNSIGNED,
+                             href VARCHAR(500),
+                             regDate DATETIME NOT NULL,
+                             updateDate DATETIME NOT NULL
+);
+
+
+# 자격증 언급 테이블
+CREATE TABLE certSubject (
+                             id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+                             certId INT UNSIGNED NOT NULL,
+                             subjectNum SMALLINT UNSIGNED,
+                             `name` VARCHAR(50),
+                             regDate DATETIME NOT NULL,
+                             updateDate DATETIME NOT NULL
+);
+
+# 자격증 테이블
+CREATE TABLE jobCat (
+                        id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+                        `name` VARCHAR(50) NOT NULL,
+                        regDate DATETIME NOT NULL,
+                        updateDate DATETIME NOT NULL
+);
+
+# 자격증 시험 테이블
+CREATE TABLE jobCode (
+                         id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+                         jobCatId INT UNSIGNED NOT NULL,
+                         jobCatName VARCHAR(50) NOT NULL,
+                         `code` INT UNSIGNED NOT NULL,
+                         `name` VARCHAR(50) NOT NULL,
+                         regDate DATETIME NOT NULL,
+                         updateDate DATETIME NOT NULL
+);
+
+
+# 자격증 언급 테이블
+CREATE TABLE certMention (
+                             id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+                             jobCatId INT UNSIGNED NOT NULL,
+                             jobCodeId INT UNSIGNED NOT NULL,
+                             certId INT UNSIGNED NOT NULL,
+                             gno INT UNSIGNED NOT NULL,
+                             `source` VARCHAR(20) NOT NULL DEFAULT "jobkorea",
+                             regDate DATETIME NOT NULL,
+                             updateDate DATETIME NOT NULL
+);
 
 
 # 회원 테이블
@@ -44,8 +164,7 @@ CREATE TABLE `member`(
                          delDate DATETIME COMMENT '탈퇴날짜'
 );
 
-
-# admin 회원 데이터 추가
+# 직무 카테고리 테이블
 INSERT INTO `member`
 SET regDate = NOW(),
     updateDate = NOW(),
@@ -58,7 +177,7 @@ SET regDate = NOW(),
     cellPhone = '01012345678',
     email = 'admin@spring.com';
 
-# 회원 테이블 데이터 추가
+# 직무 코드 테이블
 INSERT INTO `member`
 SET regDate = NOW(),
     updateDate = NOW(),
@@ -71,146 +190,34 @@ SET regDate = NOW(),
     email = CONCAT('member1', '@gmail.com');
 
 
-
-# 직무 카테고리 테이블
-CREATE TABLE jobCat (
-                        id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
-                        `name` VARCHAR(50) NOT NULL,
-                        regDate DATETIME NOT NULL,
-                        updateDate DATETIME NOT NULL
-);
-
-# 직무 코드 테이블
-CREATE TABLE jobCode (
-                         id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
-                         jobCatId INT UNSIGNED NOT NULL,
-                         jobCatName VARCHAR(50) NOT NULL,
-                         `code` INT UNSIGNED NOT NULL,
-                         `name` VARCHAR(50) NOT NULL,
-                         regDate DATETIME NOT NULL,
-                         updateDate DATETIME NOT NULL
-);
-
-
-# 자격증 테이블
-CREATE TABLE certificate (
-                             id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
-                             `name` VARCHAR(50) NOT NULL,
-                             certGrade INT UNSIGNED,
-                             isNational TINYINT COMMENT '0=국가공인민간자격 1 = 국가기술자격, -1 = 민간자격',
-                             agency VARCHAR(100),
-                             parentId INT UNSIGNED,
-                             href VARCHAR(500),
-                             regDate DATETIME NOT NULL,
-                             updateDate DATETIME NOT NULL
-);
-
-# 자격증 시험 테이블
-CREATE TABLE exam (
-                      id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
-                      certId INT UNSIGNED NOT NULL,
-                      category CHAR(10) NOT NULL,
-                      examDate DATE NOT NULL,
-                      regDate DATETIME NOT NULL,
-                      updateDate DATETIME NOT NULL
-);
-
-# 자격증 문제 테이블
-CREATE TABLE questions (
-                           id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
-                           certId INT UNSIGNED,
-                           examId INT UNSIGNED,
-                           subjectId INT UNSIGNED,
-                           questNum SMALLINT UNSIGNED NOT NULL,
-                           `body` TEXT,
-                           hasImage BOOLEAN DEFAULT FALSE,
-                           imgUrl VARCHAR(255),
-                           regDate DATETIME NOT NULL,
-                           updateDate DATETIME NOT NULL,
-
-                           INDEX idx_exam_subject (examId, subjectId)
-);
-
-# 자격증 시험 보기 테이블
-CREATE TABLE choices (
-                         id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
-                         questId INT UNSIGNED NOT NULL,
-                         label TINYINT UNSIGNED NOT NULL,
-                         `body` TEXT NOT NULL,
-                         isCorrect BOOLEAN NOT NULL,
-                         regDate DATETIME NOT NULL,
-                         updateDate DATETIME NOT NULL
-);
-
-
-# 자격증 과목 테이블
-CREATE TABLE certSubject (
-                             id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
-                             certId INT UNSIGNED NOT NULL,
-                             subjectNum SMALLINT UNSIGNED,
-                             `name` VARCHAR(50),
-                             regDate DATETIME NOT NULL,
-                             updateDate DATETIME NOT NULL
-);
-
-
-# 자격증 언급 테이블
-CREATE TABLE certMention (
-                             id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
-                             jobCatId INT UNSIGNED NOT NULL,
-                             jobCodeId INT UNSIGNED NOT NULL,
-                             certId INT UNSIGNED NOT NULL,
-                             gno INT UNSIGNED NOT NULL,
-                             `source` VARCHAR(20) NOT NULL DEFAULT "jobkorea",
-                             regDate DATETIME NOT NULL,
-                             updateDate DATETIME NOT NULL
-);
-
-
-# 자격증-직무 관계 테이블
-CREATE TABLE jobCertRel (
+# 회원 자격증 테이블
+CREATE TABLE memberCert (
                             id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
-                            jobCodeId INT UNSIGNED NOT NULL,
+                            memberId INT UNSIGNED NOT NULL,
+                            certId INT UNSIGNED,
+                            certName VARCHAR(100) NOT NULL,
+                            startDate DATE COMMENT '취득일',
+                            endDate DATE COMMENT '만료일',
+                            regDate DATETIME NOT NULL,
+                            updateDate DATETIME NOT NULL,
+                            certificateNumber VARCHAR(100) COMMENT '자격번호',
+                            alert BOOLEAN NOT NULL DEFAULT ture COMMENT '알림 설정'
+);
+
+# 회원 시험 기록 테이블
+CREATE TABLE memberExam (
+                            id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+                            memberId INT UNSIGNED NOT NULL,
                             certId INT UNSIGNED NOT NULL,
+                            examId INT UNSIGNED,
+                            elapsedTime TIME NOT NULL,
+                            totalQuest INT UNSIGNED NOT NULL,
+                            correctQuest INT UNSIGNED NOT NULL,
                             regDate DATETIME NOT NULL,
                             updateDate DATETIME NOT NULL
 );
 
 
-# 댓글 테이블
-CREATE TABLE `comment` (
-                           id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
-                           memberId INT UNSIGNED NOT NULL,
-                           relTypeCode VARCHAR(20) NOT NULL,
-                           relId INT UNSIGNED NOT NULL,
-                           `body` TEXT NOT NULL,
-                           regDate DATETIME NOT NULL,
-                           updateDate DATETIME NOT NULL
-);
-
-
-# 리액션 테이블
-CREATE TABLE reaction (
-                          id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
-                          relTypeCode VARCHAR(20) NOT NULL COMMENT '관련 데이터 타입 코드',
-                          relId INT UNSIGNED NOT NULL COMMENT '관련 데이터 id',
-                          memberId INT UNSIGNED NOT NULL,
-                          `point` TINYINT(1) NOT NULL COMMENT '1 = 좋아요, 0 = 좋아요 취소',
-                          regDate DATETIME NOT NULL,
-                          updateDate DATETIME NOT NULL
-);
-
-
-# 게시판 테이블
-CREATE TABLE board (
-                       id TINYINT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
-                       `code` VARCHAR(20) NOT NULL,
-                       `name` VARCHAR(20) NOT NULL,
-                       regDate DATETIME NOT NULL,
-                       updateDate DATETIME NOT NULL,
-                       delStatus TINYINT(1) NOT NULL DEFAULT 0 COMMENT '0 = 존재, 1 = 삭제',
-                       delDate DATETIME
-);
 
 # 게시판 테이블 데이터 추가
 INSERT INTO board SET
@@ -243,19 +250,6 @@ INSERT INTO board SET
                       regdate = NOW(),
                       updatedate = NOW();
 
-# 회원 자격증 테이블
-CREATE TABLE memberCert (
-                            id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
-                            memberId INT UNSIGNED NOT NULL,
-                            certId INT UNSIGNED,
-                            certName VARCHAR(100) NOT NULL,
-                            startDate DATE COMMENT '취득일',
-                            endDate DATE COMMENT '만료일',
-                            regDate DATETIME NOT NULL,
-                            updateDate DATETIME NOT NULL,
-                            certificateNumber VARCHAR(100) COMMENT '자격번호',
-                            alert BOOLEAN NOT NULL DEFAULT ture COMMENT '알림 설정'
-);
 
 
 INSERT INTO memberCert SET
